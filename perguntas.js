c57a47cae65e252cafb80af04fb60d9fe82ae002
finha <163540179+raphaellarodrigues2024@users.noreply.github.com>
2025-06-27 09:23:52 -0300

    d

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'programação'
+    'Como imprimir "Olá, Mundo!" na tela usando Python?'
+    '# Imprimir "Olá, Mundo!" na tela
+print("Olá, Mundo!")'
+
+)
+
+criaCartao(
+    'programação',
+    'o que significa HTML?',
+    'HyperText Markup Language'
+)
+
+criaCartao(
+    'matematica',
+    'Qual é a soma de 25 e 17?',
+    '42'
+)
+
+
